ee285b3b1ceb1c03b144a788aaf099ec69c673d9
Joey Santana <joey@joeysantana.net>
2025-02-01 01:22:17 -0800

    fixing template paths

--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    return render(request, 'templates/index.html')
+    return render(request, 'index.html')
